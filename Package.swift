4d9d17d6efca12398de2b0a4c9c5a4eacc556e03
Bouke Haarsma <bouke@haarsma.eu>
2019-03-31 11:35:59 +0200

    Upgrade to 4.2 SwiftPM manifest

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
-    name: "Evergreen"
+    name: "Evergreen",
+    products: [.library(name: "Evergreen", targets: ["Evergreen"])],
+    targets: [.target(name: "Evergreen")]
 )
